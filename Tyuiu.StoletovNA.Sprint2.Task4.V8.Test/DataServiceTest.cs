8f73975bfa29a8a3e172e5137c01c44e701c25bf
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-10-26 00:56:45 +0500

    Task4

---
+++ Tyuiu.StoletovNA.Sprint2.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.StoletovNA.Sprint2.Task4.V8.Lib;
+namespace Tyuiu.StoletovNA.Sprint2.Task4.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = -1.5;
+            Assert.AreEqual(res, ds.Calculate(x, y));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 6.0;
+            double y = -4.0;
+            var res = 15712.007;
+            Assert.AreEqual(res, ds.Calculate(x, y));
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint2.Task4.V8/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.StoletovNA.Sprint2.Task4.V8.Lib;
+namespace Tyuiu.StoletovNA.Sprint2.Task4.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тернарный оператор                                                      *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #8                                                            *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу, которая вычисляет требуемое значение с использованием*");
+            Console.WriteLine("*м тернарного оператор, где пользователь вводит значение переменных x,y с *");
+            Console.WriteLine("* клавиатуры.                                                             *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            var result = ds.Calculate(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
