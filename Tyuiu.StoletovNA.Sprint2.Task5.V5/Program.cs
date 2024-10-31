88178d7e7a599964855c300308c1b524a20fc2de
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-10-31 20:47:23 +0500

    Task5

---
+++ Tyuiu.StoletovNA.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,58 @@
+﻿using Tyuiu.StoletovNA.Sprint2.Task5.V5.Lib;
+namespace Tyuiu.StoletovNA.Sprint2.Task5.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Оператор switch                                                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                            *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу, которая использует оператор switch вычисляет требуемо*");
+            Console.WriteLine("*ое значение и возвращает результат.                                      *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            
+
+            int value;
+            Console.WriteLine("Введите номер карты:");
+            value = Convert.ToInt32(Console.ReadLine());
+            var result = ds.FindCardValue(value);
+
+            if ((value < 6) || (value > 14))
+            {
+                result = "Введен неверный номер карты";
+            }
+            else
+            {
+                result = $"Это {result}";
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(result);
+            
+            Console.ReadKey();
+        }
+    }
+}
